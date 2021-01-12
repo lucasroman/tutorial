471ee5f7c10742365b60751745f027434431f260
Lucas Roman <lucasroman@gmail.com>
2021-01-12 07:41:12 -0300

    Add route to 'react' page

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/react', function () {
+//     return view('react');
+// });
+
+Route::view('/react', 'react');
