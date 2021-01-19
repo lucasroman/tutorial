461111c4b19d61bf6e22e0a5d8a434040c1ba5da
Lucas Roman <lucasroman@gmail.com>
2021-01-19 16:45:18 -0300

    Add exclamation mark

--- public/js/exampleDeleteMe.js
+++ public/js/exampleDeleteMe.js
@@ -4,7 +4,7 @@ var greeting = document.getElementById('reactScript').getAttribute('greeting');
 function tick() {
     const element = (
         <div>
-            <h1>Hello, world</h1>
+            <h1>Hello, world!</h1>
             <h2>It is {new Date().toLocaleTimeString()}.</h2>
             <h3>{greeting}</h3>
         </div>
