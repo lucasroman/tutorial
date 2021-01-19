eeafa92be3847fb21212950bc6d28f1b153a25d4
Lucas Roman <lucasroman@gmail.com>
2021-01-19 16:44:05 -0300

    Add style to example react component

--- resources/views/react.blade.php
+++ resources/views/react.blade.php
@@ -2,13 +2,18 @@
 <html lang="en" dir="ltr">
     <head>
         <meta charset="utf-8">
-        <title>React JS</title>
+        <title>Testing React JS</title>
 
     </head>
     <body>
-        {{-- <div id="in" style="display: none;">{{ $greeting }} </div> --}}
-
-        <div id="root"></div>
+        React component
+        <div id="root" style="outline:solid;
+                        background-color:black;
+                        color:#5ed3f3;
+                        width:50%;
+                        padding:1em;
+                        margin:1em;">
+        </div>
 
         {{-- React JS --}}
         <script src="https://unpkg.com/react@17/umd/react.production.min.js" crossorigin></script>
