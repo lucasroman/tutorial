5fc4f3f0921b511c86246113d6314b75f3450903
Lucas Roman <lucasroman@gmail.com>
2021-02-09 10:17:37 -0300

    Add basic html user form to create view

---
+++ resources/views/users/create.blade.php
@@ -0,0 +1,9 @@
+<form action="#">
+    <label>First name: </label><br>
+        <input type="text" name="firstName"><br>
+
+    <label>Last name: </label><br>
+        <input type="text" name="lastName"><br>
+
+    <input type="submit" name="create">
+</form>
