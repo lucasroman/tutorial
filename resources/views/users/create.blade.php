f2a4e52cc4a55037b2349abfd639692e47ab8084
Lucas Roman <lucasroman@gmail.com>
2021-02-09 11:28:07 -0300

    Apply layout to create user view

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,3 +1,6 @@
+@extends('layouts/user')
+
+@section('content')
 <form action="#">
     <label>First name: </label><br>
         <input type="text" name="firstName"><br>
@@ -7,3 +10,9 @@
 
     <input type="submit" name="create">
 </form>
+@env('local')
+    <div>
+        You are on <span class="blink_me">local</span> environment.
+    </div>
+@endenv
+@endsection
