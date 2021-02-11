48c4ed258304a6d29329bc98ca1199567be59661
Lucas Roman <lucasroman@gmail.com>
2021-02-11 16:23:53 -0300

    Complete form react component
    - Use two attributes: first name and last name.
    - Bind function (required by javaScript).
    - Activate react functions in form.

--- public/js/userForm.js
+++ public/js/userForm.js
@@ -1,17 +1,37 @@
 class UserForm extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {value: ''};
+        this.state = {firstName: '', lastName: ''};
+
+        this.handleChangeFirstName = this.handleChangeFirstName.bind(this);
+        this.handleChangeLastName = this.handleChangeLastName.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChangeFirstName(event) {
+        this.setState({firstName: event.target.value});
+    }
+
+    handleChangeLastName(event) {
+        this.setState({lastName: event.target.value});
+    }
+
+    handleSubmit(event) {
+        alert("First name: " + this.state.firstName +
+            "\nLast name: " + this.state.lastName);
+        event.preventDefault();
     }
 
     render() {
         return (
-            <form action="#">
+            <form onSubmit={this.handleSubmit}>
                 <label>First name: </label><br />
-                <input type="text" name="firstName" /><br />
+                <input type="text" name="firstName" value={this.state.firstName}
+                    onChange={this.handleChangeFirstName} /><br />
 
                 <label>Last name: </label><br />
-                <input type="text" name="lastName" /><br />
+                <input type="text" name="lastName" value={this.state.lastName}
+                    onChange={this.handleChangeLastName}/><br />
 
                 <input type="submit" name="create" />
             </form>
