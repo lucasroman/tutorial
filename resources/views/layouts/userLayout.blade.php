2b26da76e097ad76c86998b8fbfe469b0b5ab668
Lucas Roman <lucasroman@gmail.com>
2021-03-17 11:58:32 -0300

    Remove React on CDN and Babel
    Load these libraries by CDN is not necessary anymore because React it's
    in the app now.

--- resources/views/layouts/userLayout.blade.php
+++ resources/views/layouts/userLayout.blade.php
@@ -16,12 +16,5 @@
         </div>
 
         <footer><i>Developed by <b>Lucas Roman</b></i></footer>
-
-        {{-- React JS --}}
-        <script src="https://unpkg.com/react@17/umd/react.production.min.js" crossorigin></script>
-        <script src="https://unpkg.com/react-dom@17/umd/react-dom.production.min.js" crossorigin></script>
-
-        {{-- Babel JS --}}
-        <script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
     </body>
 </html>
