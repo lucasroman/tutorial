a5158c0b2099fe6df819e409254d7ac61da5f619
Lucas Roman <lucasroman@gmail.com>
2021-04-24 22:37:04 -0300

    Remove basic react component
    This component is only for testing purpose, can be delete.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ReactController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,8 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/react', ReactController::class);
 
 Route::get('/users/create', [UserController::class, 'create']);
 
