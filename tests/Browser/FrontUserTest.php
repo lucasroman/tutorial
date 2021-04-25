ae7203a047db1f2b51cceef45c6dc88df198bbd0
Lucas Roman <lucasroman@gmail.com>
2021-04-25 11:12:17 -0300

    Delete unnecessary email test
    I am testing all field errors in back-end. Is not necessary do it here.

--- tests/Browser/FrontUserTest.php
+++ tests/Browser/FrontUserTest.php
@@ -11,17 +11,4 @@ class FrontUserTest extends DuskTestCase
 {
     use DatabaseMigrations;
 
-    // Create view should show a form and redirect to same page after submit
-    public function testShouldRedirectToAfterCreateUser()
-    {
-        $user = User::factory()->make();
-
-        $this->browse(function (Browser $browser) use ($user) {
-            $browser->visit('/users/create')
-                    ->type('firstName', $user->firstName)
-                    ->type('lastName', $user->lastName)
-                    ->press('create')
-                    ->assertDialogOpened("First name: {$user->firstName}\nLast name: {$user->lastName}");
-        });
-    }
 }
