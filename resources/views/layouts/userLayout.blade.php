4bef752dd9aa36706c14050f78a742616c296cec
Lucas Roman <lucasroman@gmail.com>
2021-05-07 11:42:37 -0300

    Move bootstrap script to end file

--- resources/views/layouts/userLayout.blade.php
+++ resources/views/layouts/userLayout.blade.php
@@ -2,13 +2,14 @@
 <html lang="en" dir="ltr">
     <head>
         <meta charset="utf-8">
-        <title>Tutorial React JS</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        {{-- Bootstrap CSS --}}
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+        {{-- Own style --}}
         <link rel="stylesheet" href="{{ asset('css/tutorial.css') }}">
 
-        {{-- Bootstrap --}}
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
-
+        <title>Tutorial React JS @yield('title', '(Untitled)')</title>
     </head>
     <body>
         <div>
@@ -16,5 +17,8 @@
         </div>
 
         <footer><i>Developed by <b>Lucas Roman</b></i></footer>
+
+        {{-- Bootstrap script (bundle) --}}
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
     </body>
 </html>
