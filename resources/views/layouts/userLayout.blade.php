71084dfeecce81ad61fa8253f81962bb84bc9c8c
Lucas Roman <lucasroman@gmail.com>
2021-05-09 11:56:06 -0300

    Update user layout
    - Add Font Awesome pro 5.15.3
    - Add navbar
    - Update footer

--- resources/views/layouts/userLayout.blade.php
+++ resources/views/layouts/userLayout.blade.php
@@ -6,17 +6,43 @@
 
         {{-- Bootstrap CSS --}}
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
+        {{-- Font Awesome Pro 5.15.3 --}}
+        <link href="https://cdn.jsdelivr.net/gh/hung1001/font-awesome-pro@0ac23ca/css/all.css" rel="stylesheet" type="text/css" />
+
         {{-- Own style --}}
         <link rel="stylesheet" href="{{ asset('css/tutorial.css') }}">
 
         <title>Tutorial React JS @yield('title', '(Untitled)')</title>
     </head>
     <body>
-        <div>
+        <nav class="navbar navbar-dark bg-dark mb-7 fixed-top">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="{{ route('welcome') }}">
+                    <i class="fab fa-react fa-stack-1x fa-inverse fa-fw"
+                        style="color: #61dafb;
+                            font-size: 1.5em;
+                            text-align: inherit;
+                            padding-left: 2em;">
+                    </i>
+                    <h3 style="color:white;
+                            padding-left: 4em;
+                            padding-top: 0.1em;">
+                        React JS examples
+                    </h3>
+                </a>
+            </div>
+        </nav>
+
+        <div class="container mt-5 py-5">
             @yield('content')
         </div>
 
-        <footer><i>Developed by <b>Lucas Roman</b></i></footer>
+        <footer class="footer py-3 mt-5 bg-dark fixed-bottom">
+            <div class="container">
+                <i>Developed by <b>Lucas Roman</b></i>
+            </div>
+        </footer>
 
         {{-- Bootstrap script (bundle) --}}
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
