aad50552dddfb21653273dd1331095a8d8594d8d
Lucas Roman <lucasroman@gmail.com>
2021-05-09 11:59:35 -0300

    Update welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 
-    <div class="container px-4 py-5">
+    <div class="px-4">
         <h2 class="pb-2 border-bottom">Active features</h2>
         <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
             <a class="anchor-without-style" href="{{ route('users.create')}}">
