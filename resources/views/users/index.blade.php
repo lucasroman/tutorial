fa9c9cb5937436683373e21a3752b116a7ca2517
Lucas Roman <lucasroman@gmail.com>
2021-05-13 11:56:41 -0300

    Update user index view to show all users

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -8,10 +8,12 @@
             <th>Last Name</th>
             <th>Email</th>
         </tr>
-        <tr>
-            <td>{{ $users->first()->first_name }}</td>
-            <td>{{ $users->first()->last_name }}</td>
-            <td>{{ $users->first()->email }}</td>
-        </tr>
+        @foreach ($users as $user)
+            <tr>
+                <td>{{ $user->first_name }}</td>
+                <td>{{ $user->last_name }}</td>
+                <td>{{ $user->email }}</td>
+            </tr>
+        @endforeach
     </table>
 @endsection
