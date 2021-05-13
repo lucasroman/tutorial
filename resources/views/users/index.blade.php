eb54d9057c34c5eae41d2e834cbf7f5e522a1b66
Lucas Roman <lucasroman@gmail.com>
2021-05-13 11:12:01 -0300

    Add table for show user data

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -2,8 +2,16 @@
 
 @section('content')
 
-    @php
-        print_r($users->first());
-    @endphp
-
+    <table class="table">
+        <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+        </tr>
+        <tr>
+            <td>{{ $users->first()->first_name }}</td>
+            <td>{{ $users->first()->last_name }}</td>
+            <td>{{ $users->first()->email }}</td>
+        </tr>
+    </table>
 @endsection
