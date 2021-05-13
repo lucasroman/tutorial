2b4937ec1a2761f25e600f8ef70a8d544c5d746b
Lucas Roman <lucasroman@gmail.com>
2021-05-13 10:31:31 -0300

    Fix test class name
    Previous names doesn't pass PSR-4 standard.

--- tests/Feature/TodoListBackTest.php
+++ tests/Feature/TodoListBackTest.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class todoListTest extends TestCase
+class TodoListBackTest extends TestCase
 {
     // Must exist todo list route and view
     public function testItCanSeeToDoListPage()
