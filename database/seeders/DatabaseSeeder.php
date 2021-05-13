eba3503a9ba2d30792e05e6a8b5fd6184b52d29a
Lucas Roman <lucasroman@gmail.com>
2021-05-13 11:51:40 -0300

    Add truncate table function to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +15,27 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        /* 'php artisan db:seed' doesn't work if exist previous unique records,
+         * you need delete all records before to run seeder, the
+         * 'truncateTables' function has that objetive.
+         */
+        $this->truncateTables([
+            'users',
+        ]);
+
+        // Run seeders for each class below
+        $this->call([
+            UserSeeder::class,
+        ]);
+
+    }
+
+    protected function truncateTables(array $tables)
+    {
+        // Delete all records on each $table
+        foreach ($tables as $table) {
+            DB::table($table)->truncate();
+        }
     }
 }
