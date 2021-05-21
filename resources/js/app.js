589d8f677d24b7f131d00fbb52f407b6b9a807e7
Lucas Roman <lucasroman@gmail.com>
2021-05-21 20:52:26 -0300

    Make User List Component
    - Register component in app.js file.
    - Add an id in the index view to display the component.
    - Make basic structure for this component.

--- resources/js/app.js
+++ resources/js/app.js
@@ -15,3 +15,4 @@ require('./bootstrap');
 require('./components/Example');
 require('./components/userForm');
 require('./components/todoList');
+require('./components/userList');
---
+++ resources/js/components/usersList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class UserData extends React.Component {
+
+}
+
+class UserList extends React.Component {
+
+}
+
+if (document.getElementById('userList')) {
+    RactDOM.render(
+        <UserList />,
+        document.getElementById('userList')
+    );
+}
