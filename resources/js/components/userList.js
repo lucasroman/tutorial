c41ee33d895607cd52dfce1799115a7a13b39f92
Lucas Roman <lucasroman@gmail.com>
2021-05-26 10:34:16 -0300

    Make users table wit react component

--- resources/js/components/userList.js
+++ resources/js/components/userList.js
@@ -2,11 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class UserList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { users: JSON.parse(container.dataset.users) };
+    }
+
     render() {
         return (
             <div>
                 <h3>Here users table</h3>
-                <UserData />
+
+                <table className="table">
+                    <tr>
+                        <th>Last Name</th>
+                        <th>First Name</th>
+                        <th>Email</th>
+                    </tr>
+
+                    <UserData users={users} />
+                </table>
             </div>
 
         );
@@ -14,16 +28,31 @@ class UserList extends React.Component {
 }
 
 class UserData extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { users: JSON.parse(container.dataset.users) };
+    }
     render() {
         return (
-            <p>'Row with user data'</p>
+            <>
+            {this.state.users.map(user => (
+                <tr>
+                    <td key={Date.now()}>{user.last_name}</td>
+                    <td key={Date.now()}>{user.first_name}</td>
+                    <td key={Date.now()}>{user.email}</td>
+                </tr>
+            ))}
+            </>
         );
     }
 }
 
-if (document.getElementById('userList')) {
+var container = document.getElementById('userList');
+var users = container.dataset.users;
+
+if (container) {
     ReactDOM.render(
-        <UserList />,
-        document.getElementById('userList')
+        <UserList users={users} />,
+        container
     );
 }
