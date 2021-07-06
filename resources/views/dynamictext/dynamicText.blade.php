2446f33e6df89e016402c1a2810c5d735bc2a351
Lucas Roman <lucasroman@gmail.com>
2021-07-05 21:11:03 -0300

    Add new folder for dynamic text views
    Also add the first view for this component.

---
+++ resources/views/dynamictext/dynamicText.blade.php
@@ -0,0 +1,6 @@
+<h3 style="font-family:arial">
+    <span style="color:#00ff00">&#10003;</span> Url is working! &#128077;
+</h3>
+<h3 style="font-family:arial">
+    <span style="color:#00ff00">&#10003;</span> View is working! &#128077;
+</h3>
