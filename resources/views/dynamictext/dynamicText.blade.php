cbf176332954f2e566ca78ee5d50896a3209469b
Lucas Roman <lucasroman@gmail.com>
2021-07-09 20:48:13 -0300

    Add layout to dynamic text view

--- resources/views/dynamictext/dynamicText.blade.php
+++ resources/views/dynamictext/dynamicText.blade.php
@@ -1,8 +1,12 @@
-<h3 style="font-family:arial">
-    <span style="color:#00ff00">&#10003;</span> Url is working! &#128077;
-</h3>
-<h3 style="font-family:arial">
-    <span style="color:#00ff00">&#10003;</span> View is working! &#128077;
-</h3>
+@extends('layouts/userLayout')
 
-<input name="input-text">
+@section('content')
+    <h3 style="font-family:arial">
+        <span style="color:#00ff00">&#10003;</span> Url is working! &#128077;
+    </h3>
+    <h3 style="font-family:arial">
+        <span style="color:#00ff00">&#10003;</span> View is working! &#128077;
+    </h3>
+
+    <div id="dynamicText"></div>
+@endsection
