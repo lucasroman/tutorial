02f0a7d5bcb2f6f2d949ab43007e7b444ec503c0
Lucas Roman <lucasroman@gmail.com>
2021-07-09 20:49:09 -0300

    Compile files

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,5 +13,4 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .react()
-    .sass('resources/sass/app.scss', 'public/css')
-    .postCss('resources/css/tutorial.css', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css');
