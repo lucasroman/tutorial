75404c87eb8b8b60e8ce314220100f8f09f4ba4f
Lucas Roman <lucasroman@gmail.com>
2021-07-14 21:08:35 -0300

    Remove old html table
    Now the user component make that work.

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,21 +1,7 @@
 @extends('layouts/userLayout')
 
 @section('content')
+    
     <div id="userList" data-users="{{ $users }}"></div>
-
-    {{-- <table class="table">
-        <tr>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Email</th>
-        </tr>
-        @foreach ($users as $user)
-            <tr>
-                <td>{{ $user->first_name }}</td>
-                <td>{{ $user->last_name }}</td>
-                <td>{{ $user->email }}</td>
-            </tr>
-        @endforeach
-    </table> --}}
 
 @endsection
