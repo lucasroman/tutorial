69cc48b2f72ccc2b2b210c772f5576eb37def382
Lucas Roman <lucasroman@gmail.com>
2021-08-17 19:48:15 -0300

    Fix classnames and htmlFor for React format

--- resources/js/components/userForm.js
+++ resources/js/components/userForm.js
@@ -2,57 +2,42 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class UserForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {lastName: ''};
-
-        // this.handleChangeFirstName = this.handleChangeFirstName.bind(this);
-        this.handleChangeLastName = this.handleChangeLastName.bind(this);
-    }
-
-    // handleChangeFirstName(event) {
-    //     this.setState({firstName: event.target.value});
-    // }
-
-    handleChangeLastName(event) {
-        this.setState({lastName: event.target.value});
-    }
-
     render() {
         return (
             <form action="/users" method="post">
-                <div class="row mb-3">
-                    <div class="form-group col">
-                        <label for="firstName" class="form-label">
+                <div className="row mb-3">
+                    <div className="form-group col">
+                        <label htmlFor="firstName" className="form-label">
                             First name
                         </label>
-                        <input type="text" class="form-control" name="firstName"
-                            id="firstName"/>
+                        <input type="text" className="form-control"
+                             name="firstName" id="firstName"/>
                     </div>
 
-                    <div class="form-group col">
-                        <label for="lastName" class="form-label">
+                    <div className="form-group col">
+                        <label htmlFor="lastName" className="form-label">
                             Last name
                         </label>
-                        <input type="text" class="form-control" name="lastName"
-                            value={this.state.lastName}
-                            onChange={this.handleChangeLastName} id="lastName"/>
+                        <input type="text" className="form-control" name="lastName" id="lastName"/>
                     </div>
                 </div>
 
-                <div class="form-group mb-3">
-                    <label for="email" class="form-label">Email</label>
-                    <input type="email" class="form-control" name="email"
-                        id="email"/>
+                <div className="form-group mb-3">
+                    <label htmlFor="email" className="form-label">Email</label>
+                    <input type="email" className="form-control"
+                        name="email" id="email"/>
                 </div>
 
-                <input type="submit" value="Create User" class="btn btn-primary
-                    mb-3" />
+                <input type="submit" value="Create User"
+                    className="btn btn-primary mb-3" />
             </form>
         );
     }
 }
 
 if (document.getElementById('userForm')) {
-    ReactDOM.render(<UserForm />, document.getElementById('userForm'));
+    ReactDOM.render(
+        <UserForm />,
+        document.getElementById('userForm')
+    );
 }
